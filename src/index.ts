51ec97a37f1d4cf2e9fffd844b42ee535a7cb9ff
csabadaniel <csaba.daniel@db-soft.hu>
2025-07-05 19:31:17 +0100

    Add YNAB API integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, YNAB!");
+import 'dotenv/config';
+import * as ynab from 'ynab';
+
+const accessToken = process.env.YNAB_ACCESS_TOKEN!;
+const ynabAPI = new ynab.API(accessToken);
+
+async function fetchDefaultBudget() {
+  try {
+    const budgetsResponse = await ynabAPI.budgets.getBudgets();
+    const defaultBudget = budgetsResponse.data.budgets[0];
+    
+    console.log(JSON.stringify(budgetsResponse, null, 2));
+  } catch (error) {
+    console.error('Error fetching budgets:', error);
+  }
+}
+
+fetchDefaultBudget();
